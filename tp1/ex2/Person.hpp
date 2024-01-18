ef85439b6aaf5c80b14f6505577259c50efcabe2
axel-r-6 <a.robert.pro2@gmail.com>
2024-01-18 23:34:45 +0100

    ex2

--- tp1/ex2/Person.hpp
+++ tp1/ex2/Person.hpp
@@ -2,7 +2,26 @@
 
 class Person
 {
+
 public:
-    std::string first_name;
-    std::string surname;
+    Person()
+    {
+    }
+
+    Person(const std::string &firstname, const std::string &surname)
+        : _first_name(firstname), _surname(surname){};
+
+    std::string get_first_name() const
+    {
+        return _first_name;
+    };
+
+    std::string get_surname() const
+    {
+        return _surname;
+    };
+
+private:
+    std::string _first_name;
+    std::string _surname;
 };
--- tp1/ex2/main.cpp
+++ tp1/ex2/main.cpp
@@ -3,9 +3,49 @@
 
 int main()
 {
+    /*
     Person p;
     p.first_name = "Palluche";
     p.surname = "La Faluche";
 
     std::cout << "Person named " << p.first_name << " " << p.surname << std::endl;
+    */
+    /*
+    Person p("Paluche", "La Faluche");
+
+    std::cout << "Person named : " << p.get_first_name() << " " << p.get_surname() << std::endl;
+    */
+
+    unsigned int number;
+
+    do
+    {
+
+        std::cout << "Nombre de personnes: ";
+        std::cin >> number;
+
+    } while (number < 0);
+
+    std::vector<Person> persons;
+
+    for (size_t i = 0; i < number; i++)
+    {
+        std::string first_name;
+        std::string surname;
+        std::cout << "Prenom :";
+        std::cin >> first_name;
+        std::cout << "Nom :";
+        std::cin >> surname;
+        Person p(first_name, surname);
+        persons.push_back(p);
+    }
+
+    std::cout << "Les personnes sont ";
+    for (size_t i = 0; i < number; i++)
+    {
+        std::cout << persons[i].get_first_name() << " " << persons[i].get_surname();
+
+        std::cout << ((i < number - 1) ? ", " : ".");
+    }
+    std::cout << std::endl;
 }
