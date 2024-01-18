ec821a3aa9a2eb6145814994a797c55ce39bc658
axel-r-6 <a.robert.pro2@gmail.com>
2024-01-18 11:08:04 +0100

    change char to string

--- tp1/ex1/main.cpp
+++ tp1/ex1/main.cpp
@@ -5,8 +5,10 @@ int main()
 {
     std::cout << "Entre ton nom: ";
 
-    char name[20] = "";
-    std::cin >> std::setw(20) >> name;
+    // char name[20] = "";
+    std::string name;
+    // std::cin >> std::setw(20) >> name;
+    std::cin >> name;
     std::cout << "Bonjour " << name << " !" << std::endl;
 
     return 0;
