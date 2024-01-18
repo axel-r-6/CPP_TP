d5911671a150f92a80cd777d206b269dc5996bb5
axel-r-6 <a.robert.pro2@gmail.com>
2024-01-18 11:16:57 +0100

    ex2 q3

--- tp1/ex2/main.cpp
+++ tp1/ex2/main.cpp
@@ -1,8 +1,11 @@
 #include "Person.hpp"
+#include <iostream>
 
 int main()
 {
     Person p;
     p.first_name = "Palluche";
     p.surname = "La Faluche";
+
+    std::cout << "Person named " << p.first_name << " " << p.surname << std::endl;
 }
